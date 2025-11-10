bc25378e4e7e27fa966be3423c633df57ec24c8f
Rogier Borst <rogier.borst@paqt.com>
2025-11-10 09:26:27 +0100

    lmplement a simple camera + gallery

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,24 @@
 
 import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [
+        vue(),
+        legacy(),
+        tailwindcss()
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom'
-  }
+    test: {
+        globals: true,
+        environment: 'jsdom'
+    }
 })
